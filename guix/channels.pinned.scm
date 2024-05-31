caef9ae533cda159449f7665dd527d544c27bdff
Sebastian Gibb <mail@sebastiangibb.de>
2024-05-31 23:34:10 +0200

    chore: update toolchain to latest guix commit

--- guix/channels.pinned.scm
+++ guix/channels.pinned.scm
@@ -1,17 +1,22 @@
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "90e8febd297f855e8103e91f11597ed817bb4566")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
         (name 'minai)
         (url "https://github.com/umg-minai/guix-minai.git")
         (branch "main")
         (commit
-          "3ccac41c5ebe1e660f7e5ac3f46fd6b2044af51a")))
+          "a3f00cdce7715a09af154675bad002a990cd6135")
+        (introduction
+          (make-channel-introduction
+            "4e47c0f12cf838335bf731b1947ea434923569e3"
+            (openpgp-fingerprint
+              "7EAB 7082 91D9 28E5 4387  47A7 F9F3 CB1B 50FB 607A"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "fba6896f625dcbeef112387fc90abe83acae1720")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
