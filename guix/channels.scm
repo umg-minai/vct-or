daabdf005bfc2a6c7790b32f912fec2508e35a7a
Sebastian Gibb <mail@sebastiangibb.de>
2024-05-31 23:33:38 +0200

    chore: use channel authentication for minai

--- guix/channels.scm
+++ guix/channels.scm
@@ -2,5 +2,10 @@
   (channel
     (name 'minai)
     (url "https://github.com/umg-minai/guix-minai.git")
-    (branch "main"))
+    (branch "main")
+    (introduction
+      (make-channel-introduction
+        "4e47c0f12cf838335bf731b1947ea434923569e3"
+        (openpgp-fingerprint
+          "7EAB 7082 91D9 28E5 4387  47A7 F9F3 CB1B 50FB 607A"))))
   %default-channels)
